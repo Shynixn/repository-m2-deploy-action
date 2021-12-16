97b161ffa77e571920e406f9e13b8871af478d7b
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 17:50:19 +0100

    Changed workflow.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,11 @@ mv $parameterMainJar $mainJarFile
 # Generate full folder path
 fullFolderPath=master/docs/repository/$folderPath
 
+echo "Data:"
+echo $githubUserName
+echo $githubAccessToken
+echo $https://$githubUserName:$githubAccessToken@github.com/$githubUserName/$githubRepository.git
+
 # Setup the repository
 git config --global user.email "repository-m2-deployment-agent@email.com" && git config --global user.name "Repository M2 Deployment Agent"
 git clone https://$githubUserName:$githubAccessToken@github.com/$githubUserName/$githubRepository.git master
