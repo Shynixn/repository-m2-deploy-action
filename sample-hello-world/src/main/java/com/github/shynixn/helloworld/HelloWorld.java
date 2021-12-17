eb353137c46b68e608029d7bcb6db2bd80fba3d6
Shynixn <shynixn@email.com>
2021-12-17 13:49:08 +0100

    Added signing parameters.

--- sample-hello-world/src/main/java/com/github/shynixn/helloworld/HelloWorld.java
+++ sample-hello-world/src/main/java/com/github/shynixn/helloworld/HelloWorld.java
@@ -1,7 +1,20 @@
 package com.github.shynixn.helloworld;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 public class HelloWorld {
-    public void say() {
-        System.out.println("Hello World!");
+    public void say() throws IOException {
+        byte[] data = Files.readAllBytes(Paths.get("F:\\Archive\\Z_\\Dokumente\\Keys\\2021-Latest Private PublicKey\\secret-base64-4.asc"));
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte[] data = Files.readAllBytes(Paths.get("F:\\Archive\\Z_\\Dokumente\\Keys\\2021-Latest Private PublicKey\\secret-base64-4.asc"));
+
+       Files.writeString(Paths.get("F:\\Archive\\Z_\\Dokumente\\Keys\\2021-Latest Private PublicKey\\secret-base64-5.asc"), Base64.getEncoder().encodeToString(data));
     }
 }
