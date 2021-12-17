dd8ee6b10c18847902e950bea7b476dbf0747971
Shynixn <shynixn@email.com>
2021-12-17 13:16:12 +0100

    Implemented signing.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN apt-get update
 RUN apt-get install maven -y
 RUN apt-get install git -y
 RUN apt-get install openjdk-11-jdk -y
+RUN apt-get install gnupg -y
 
 COPY entrypoint.sh entrypoint.sh
 RUN chmod +x entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ githubAccessToken=$5
 githubUserName=$6
 githubRepository=$7
 folderPath=$8
+signingKey=$9
+signingPassword=$10
 
 # Rename jar artifact to maven format
 mainJarFile="$groupId-$artifactId-$version.jar"
@@ -25,6 +27,16 @@ mkdir -p $fullFolderPath
 # Install the files into the repository
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 mvn install:install-file "-DgroupId=$groupId" "-DartifactId=$artifactId" "-Dversion=$version" "-Dfile=$mainJarFile" -Dpackaging=jar -DgeneratePom=true "-DlocalRepositoryPath=$fullFolderPath" -DcreateChecksum=true
+
+# Optional Signing
+if [ -n "$signingKey" ]; then
+  echo $signingKey > secret.asc
+  installFolder=$fullFolderPath/$(echo "$groupId" | sed -e "s~\.~/~g")/$artifactId/$version
+  echo $installFolder
+  gpg --batch --import --armor secret.asc
+  gpg --list-keys
+  find $installFolder -maxdepth 100 -not -name "*.asc" -type f -exec sh -c 'echo $1 | gpg -ab --batch --yes --passphrase-fd 0 --pinentry-mode loopback $0 && echo Signed $0.' {} $signingPassword ';'
+fi
 
 # Push the changes to Github
 cd master
